81bf8b2c4e17449e6b0e66608d7621ce4ed7381b
hexrain <nick@hexsviolet.com>
2015-04-02 01:47:07 +0300

    Removed redundant code

--- movie.py
+++ movie.py
@@ -9,14 +9,6 @@ class Movie(object):
         imdb_url: A string url pointing to the movie's imdb page
     """
 
-    trailer_youtube_url = ""
-
-    title = ""
-
-    poster_image_url = ""
-
-    imdb_url = ""
-
     def __init__(self, title, trailer, poster_url, imdb_url):
         """Inits Movie with title, trailer url, poster url and imdb_url"""
         super(Movie, self).__init__()
